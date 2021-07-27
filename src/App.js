844a3154550959360573cc846f597c415c38b16d
Devanshu <devanshudesai_1509@yahoo.com>
2021-07-27 13:45:44 +0530

    About-Me Done

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Particles from "react-particles-js";
 import NavBar from "./components/navBar";
 import Header from "./components/header";
+import i1 from "./android.svg"
+import AboutMe from "./components/about";
 
 function App() {
   return (
     <>
-      <Particles
+	<Particles
         params={{
           particles:{
             number:{
-              value:30,
+              value:50,
               density:{
                 enable: true,
                 value_area: 900
@@ -24,70 +26,23 @@ function App() {
                 color: "#e68d09"
               }
             },
-          }
+          },
+		  interactivity:{
+			  events:{
+				  onhover:{
+					  enable: true,
+					  mode: "push"
+				  }
+			  }
+		  }
         }}
       />
-      <NavBar/>
-      <Header/>
-    </>
-  );
+	<NavBar/>
+    <Header/>
+	<AboutMe />
+	</>  
+	);
 }
+  
 
 export default App;
-
-//   <Particles
-  //   params={{
-	//     "particles": {
-	//         "number": {
-	//             "value": 8,
-	//             "density": {
-	//                 "enable": true,
-	//                 "value_area": 800
-	//             }
-	//         },
-	//         "line_linked": {
-	//             "enable": false
-	//         },
-	//         "move": {
-	//             "speed": 1,
-	//             "out_mode": "out"
-	//         },
-	//         "shape": {
-	//             "type": [
-	//                 "image",
-	//                 "square"
-	//             ],
-	//             "image": [
-	//                 {
-	//                     "src": "/react.cd2ab268.svg",
-	//                     "height": 20,
-	//                     "width": 23
-	//                 },
-	//                 {
-	//                     "src": "/k8s.2d579d24.svg",
-	//                     "height": 20,
-	//                     "width": 20
-	//                 },
-	//                 {
-	//                     "src": "/code.b3b4c4f4.png",
-	//                     "height": 20,
-	//                     "width": 20
-	//                 }
-	//             ]
-	//         },
-	//         "color": {
-	//             "value": "#CCC"
-	//         },
-	//         "size": {
-	//             "value": 30,
-	//             "random": false,
-	//             "anim": {
-	//                 "enable": true,
-	//                 "speed": 4,
-	//                 "size_min": 10,
-	//                 "sync": false
-	//             }
-	//         }
-	//     },
-	//     "retina_detect": false
-	// }} />
